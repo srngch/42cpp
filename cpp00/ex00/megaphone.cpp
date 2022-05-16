9a80a8bc7282710bfaad3155f7ed0136b5888e09
srngch <srngch.dev@gmail.com>
2022-05-17 02:12:06 +0900

    style(ex00): not use norm

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: sarchoi <sarchoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 00:51:35 by sarchoi           #+#    #+#             */
-/*   Updated: 2022/05/17 01:26:14 by sarchoi          ###   ########seoul.kr  */
+/*   Updated: 2022/05/17 02:10:58 by sarchoi          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,17 @@
 #include <string>
 #include <locale>
 
-int main(int argc, char **argv)
-{
-	if (argc == 1)
-	{
+int main(int argc, char **argv) {
+	if (argc == 1) {
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return (0);
 	}
-	for (int i = 1; i < argc; i++)
-	{
+	for (int i = 1; i < argc; i++) {
 		std::locale loc;
 		std::string str = argv[i];
 		for (std::string::size_type j = 0; j < str.length(); j++)
 			std::cout << std::toupper(str[j], loc);
 	}
 	std::cout << std::endl;
-	return (0);
+	return 0;
 }
