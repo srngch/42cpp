ea292fa7d601e1743f7fb7643e0edcee45971e1d
srngch <srngch.dev@gmail.com>
2022-05-26 16:50:36 +0900

    feat(ex06): Add usage message when argument is not 2

--- cpp01/ex06/main.cpp
+++ cpp01/ex06/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sarchoi <sarchoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 16:12:32 by sarchoi           #+#    #+#             */
-/*   Updated: 2022/05/26 16:40:43 by sarchoi          ###   ########seoul.kr  */
+/*   Updated: 2022/05/26 16:49:27 by sarchoi          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,11 @@
 int main(int argc, char **argv) {
 	Harl harl;
 
-	if (argc != 2)
+	if (argc != 2) {
+		std::cout << "Usage: ./Harl <level>" << std::endl;
+		std::cout << "Levels: DEBUG, INFO, WARNING, ERROR" << std::endl;
 		return 1;
+	}
 	harl.complain(argv[1]);
 	return 0;
 }
