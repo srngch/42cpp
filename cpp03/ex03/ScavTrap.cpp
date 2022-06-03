df93d1d3a1a4cd7828ff5bb918f9e05d7fa751ea
srngch <srngch.dev@gmail.com>
2022-06-04 00:11:14 +0900

    fix(ex03): Update ScavTrap class

--- cpp03/ex03/ScavTrap.cpp
+++ cpp03/ex03/ScavTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: sarchoi <sarchoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/02 01:36:21 by sarchoi           #+#    #+#             */
-/*   Updated: 2022/06/03 21:44:15 by sarchoi          ###   ########seoul.kr  */
+/*   Updated: 2022/06/04 00:03:13 by sarchoi          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,34 +33,35 @@ ScavTrap::ScavTrap(std::string name) : ClapTrap(name) {
 }
 
 ScavTrap::ScavTrap(ScavTrap const & src){
-	std::cout << "ScavTrap " << src.name << " copied!" << std::endl;
+	std::cout << "ScavTrap " << src.getName() << " copied!" << std::endl;
 	*this = src;
 }
 
 ScavTrap & ScavTrap::operator=(ScavTrap const & rhs) {
-	std::cout << "ScavTrap " << rhs.name << " assigned!" << std::endl;
+	std::cout << "ScavTrap " << rhs.getName() << " assigned!" << std::endl;
 	if (this != &rhs) {
-		this->name = rhs.name;
-		this->hit_points = rhs.hit_points;
-		this->energy_points = rhs.energy_points;
-		this->attack_damage = rhs.attack_damage;
+		this->setName(rhs.getName());
+		this->setMaxHitPoints(rhs.getMaxHitPoints());
+		this->setHitPoints(rhs.getHitPoints());
+		this->setEnergyPoints(rhs.getEnergyPoints());
+		this->setAttackDamage(rhs.getAttackDamage());
 	}
 	return *this;
 }
 
 ScavTrap::~ScavTrap(void) {
-	std::cout << "ScavTrap " << this->name << " destroyed!" << std::endl;
+	std::cout << "ScavTrap " << this->getName() << " destroyed!" << std::endl;
 }
 
 void ScavTrap::attack(const std::string & target) {
-	if (this->hit_points <= 0 || this->energy_points <=0) {
-		std::cout << "ScavTrap " << this->name << " can't attack..." << std::endl;
+	if (this->getHitPoints() <= 0 || this->getEnergyPoints() <=0) {
+		std::cout << "ScavTrap " << this->getName() << " can't attack..." << std::endl;
 		return ;
 	}
-	std::cout << "ScavTrap " << this->name << " attacks " << target << ", causing " << this->attack_damage << " points of damage!" << std::endl;
-	this->energy_points--;
+	std::cout << "ScavTrap " << this->getName() << " attacks " << target << ", causing " << this->getAttackDamage() << " points of damage!" << std::endl;
+	this->setEnergyPoints(this->getEnergyPoints() - 1);
 }
 
 void ScavTrap::guardGate() {
-	std::cout << "ScavTrap " << this->name << " guards the gate!" << std::endl;
+	std::cout << "ScavTrap " << this->getName() << " guards the gate!" << std::endl;
 }
