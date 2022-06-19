510629827990f605059518a7d7e860608d82fe16
srngch <srngch.dev@gmail.com>
2022-06-19 16:42:26 +0900

    feat(ex00): Add ex00

---
+++ cpp07/ex00/whatever.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sarchoi <sarchoi@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/19 16:41:46 by sarchoi           #+#    #+#             */
+/*   Updated: 2022/06/19 16:41:47 by sarchoi          ###   ########seoul.kr  */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __WHATEVER_H__
+#define __WHATEVER_H__
+
+template <typename T>
+void swap(T &a, T &b)
+{
+	T tmp = a;
+	a = b;
+	b = tmp;
+}
+
+template <typename T>
+T min(T a, T b)
+{
+	return (a < b) ? a : b;
+}
+
+template <typename T>
+T max(T a, T b)
+{
+	return (a > b) ? a : b;
+}
+
+#endif /* __WHATEVER_H__ */
